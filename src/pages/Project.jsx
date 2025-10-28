dcec0465a8b96678287aa38204a999f8af8ba597
Abhinav Ellath <69187241+abhinavellath@users.noreply.github.com>
2025-10-28 21:27:22 +0530

    Update project image URL in Project.jsx

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -36,7 +36,7 @@ const projects = [
             "Python", "Linux", "Tensorflow",
         ],
         image:
-            "https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1470&q=80",
+            "https://unsplash.com/photos/man-resting-head-on-steering-wheel-inside-car-W4Ve9gOmykY",
     },
     
     {
